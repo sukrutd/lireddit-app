b41319712c56b453d6b2d7b0bb239c155eb8e0ee
sukrutd <sukrut.dd@gmail.com>
2021-02-03 12:34:52 +0530

    Added tsconfig, package.json scripts, MikroORM configuration and setup apollo server for graphql

---
+++ server/src/index.ts
@@ -0,0 +1,32 @@
+import { MikroORM } from '@mikro-orm/core';
+import mikroConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/posts';
+
+const main = async () => {
+	const orm = await MikroORM.init(mikroConfig);
+	await orm.getMigrator().up();
+
+	const app = express();
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver, PostResolver],
+			validate: false
+		}),
+		context: () => ({ em: orm.em })
+	});
+	apolloServer.applyMiddleware({ app });
+
+	const port = process.env.PORT || 5000;
+	app.listen(port, () => {
+		console.log(`Server running on port: ${port}`);
+	});
+};
+
+main().catch((error) => {
+	console.log(error);
+});
